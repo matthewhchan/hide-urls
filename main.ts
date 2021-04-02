cf67771ec647fd88d780d3ecc4f0be48e6034a47
Matthew Chan <>
2021-04-02 11:20:50 -0700

    Refresh editor when cursor moves to new line.

--- main.ts
+++ main.ts
@@ -2,8 +2,19 @@ import {App, Plugin} from 'obsidian';
 
 export default class HideUrls extends Plugin {
   async onload() {
-    this.registerCodeMirror((cm: any) => {
-      cm.setOption("styleActiveLine", {nonEmpty: true});
+    this.registerCodeMirror(
+        (cm: any) => { cm.setOption("styleActiveLine", {nonEmpty : true}); });
+
+    this.registerCodeMirror((cm: CodeMirror.Editor) => {
+      cm.on("cursorActivity", (instance: any) => {
+        let current_line_number = cm.getCursor().line;
+        if (current_line_number != this.line_number) {
+          cm.refresh();
+          this.line_number = current_line_number;
+        }
+      });
     });
   }
+
+  line_number: number;
 }
